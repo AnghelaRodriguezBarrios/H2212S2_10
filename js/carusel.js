9eed0d9b56659e2632127e659ae42dcd6c756182
AnghelaRodriguezBarrios <anghela.rodriguez@vallegrande.edu.pe>
2022-12-13 21:13:08 -0500

    server 4.0

--- js/carusel.js
+++ js/carusel.js
@@ -1,17 +1,17 @@
 $(function(){
     $('.center').slick({
         infinite:true,
-        slidesToShow: 4,
-    slidesToScroll: 1,
-        arrows:false,
-        autoplay: true,
-        autoplaySpeed: 3900,
-        dots: false,
-        centerModel:true,
-        centerPadding:'60px',
+        slidesToShow: 4, /*diapositivas para mostrar: 4*/
+    slidesToScroll: 1, /*  diapositivas para desplazarse: 1*/
+        arrows:false, /*  flechas:falso*/
+        autoplay: true, /*reproducción automática: cierto*/
+        autoplaySpeed: 3900, /*velocidad de reproducción automática: 3900,*/
+        dots: false, /* reproducción automática: 3900*/
+        centerModel:true, /*puntos: falso*/
+        centerPadding:'60px', /*modelo central: verdadero*/
         responsive: [
             {
-              breakpoint: 1024,
+              breakpoint: 1024,  /*relleno central: '60px',receptivo:*/
               settings: {
                 slidesToShow: 3,
                 slidesToScroll: 1,
