0275641d0ce41c241528297e751b2e3be7172070
AnghelaRodriguezBarrios <anghela.rodriguez@vallegrande.edu.pe>
2022-12-13 20:08:27 -0500

    texto 2.0

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (rep, res) => ('Hello world with express'))
+app.get('/', (rep, res) => ('<h1>Express or wordt </h1>'))
 
 app.listen(3000);
 console.log('server on port 3000')
